9944d37cb8e3bd9f0b3eb7a4c82472b555b4e1bd
Michal Krissak <michal.krissak@vacuumlabs.com>
2022-11-03 21:07:52 +1000

    ADD seed folder generation

--- seed/seed.groovy
+++ seed/seed.groovy
@@ -14,7 +14,7 @@ pipelineJob("${SEED_PROJECT}-${SEED_BRANCH}-build".replaceAll('/','-')) {
          scm {
             git {
                remote {
-                  url("${PROJECT_SCM_URL")
+                  url("${PROJECT_SCM_URL}")
                }
                //branches("${SEED_BRANCH}")
                extensions { }
